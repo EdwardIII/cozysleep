40d167210408cb385bc8a3655b4e5c44d986887e
Edward Prendergast <edward@adaodesign.com>
2020-11-16 19:51:49 +0000

    Clean up formatting

--- test/cozysleep/status_test.clj
+++ test/cozysleep/status_test.clj
@@ -3,6 +3,5 @@
             [cozysleep.status :as status]))
 
 (deftest check-status-200
-  (testing "check-statuses with mixed statuses"
-    (is (= 200
-           (get (status/check-status "https://www.google.com") :code)))))
+  (is (= 200
+         (get (status/check-status "https://www.google.com") :code))))
